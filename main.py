f60d756294f37a507dafa4fde7431f4ea765bc2e
Raian Barbosa <raianpbstudio@gmail.com>
2024-05-23 19:33:59 -0300

    fix javascript cors

--- main.py
+++ main.py
@@ -12,6 +12,26 @@ from orm import ormGame
 from dbconnection.connection import get_db
 db = next(get_db())
 app = FastAPI(docs_url="/", title="API Footbaal Today Odds 1.01")
+
+# Configurações de CORS
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://football-odds-api.p.rapidapi.com",
+    "http://localhost",
+    # Adicione outras origens permitidas aqui
+]
+
+app = FastAPI(docs_url="/", title="API Football Today Odds 1.01")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # credentials: JWTAUTH = Security(ACCESS_SECURITY)
 @app.get("/game/odds", tags=['OddsFootbaal'])
 def scrape_flashscore(access:int = Query(...), date: Optional[DateType] = Query(None)):
