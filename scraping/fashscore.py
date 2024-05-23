3e19a461b501833f99fe79b89217ad87661855a3
Raian Barbosa <57734572+raianb-dev@users.noreply.github.com>
2024-05-23 16:38:10 -0300

    Update fashscore.py

--- scraping/fashscore.py
+++ scraping/fashscore.py
@@ -7,13 +7,15 @@ import time
 import uuid
 import re
 import platform
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 def scrap_flashscore(url):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+
+    # Especifique o caminho do binário do Chrome
+    chrome_options.binary_location = "/usr/bin/google-chrome"  # Atualize este caminho se necessário
+
     if platform.system() == 'Linux':
         service = Service(executable_path='./chromedriver')
         driver = webdriver.Chrome(service=service, options=chrome_options)
@@ -68,7 +70,7 @@ def scrap_flashscore(url):
                         if odd_value:
                             odds_encontradas.append(odd_value.group(1) or odd_value.group(2))
 
-                    if horario_encontrado and len(times_encontrados) == 2 or horario_encontrado  and odds_encontradas:
+                    if horario_encontrado e len(times_encontrados) == 2 or horario_encontrado e odds_encontradas:
                         id = str(uuid.uuid4())
                         jogo_info = {
                             "id": f"{id}",
